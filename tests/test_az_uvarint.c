d119c8b5e40e6d6e6b1aa4e496f40ceb3a6a8a10
jmcph4 <jmcph4@users.noreply.github.com>
2018-12-25 19:26:46 +1000

    Add unit tests for `az_uvarint_decode`
    This commit adds unit tests to cover decoding unsigned variable
    integers.

--- tests/test_az_uvarint.c
+++ tests/test_az_uvarint.c
@@ -84,15 +84,115 @@ void test_az_uvarint_encode_specsample6(void)
     TEST_ASSERT_EQUAL_UINT64(1, uvarint.bytes[2]);
 }
 
+void test_az_uvarint_decode_specsample1(void)
+{
+    az_uvarint_t uvarint;
+    uvarint.len = 1;
+    uvarint.bytes = calloc(uvarint.len, sizeof(uint64_t));
+    uvarint.bytes[0] = 1;
+
+    uint64_t num = 0;
+    az_status_t res = az_uvarint_decode(uvarint, &num);
+
+    TEST_ASSERT_EQUAL_INT(AZ_STATUS_OK, res);
+    TEST_ASSERT_EQUAL_UINT64(1, num);
+}
+
+void test_az_uvarint_decode_specsample2(void)
+{
+    az_uvarint_t uvarint;
+    uvarint.len = 1;
+    uvarint.bytes = calloc(uvarint.len, sizeof(uint64_t));
+    uvarint.bytes[0] = 127;
+
+    uint64_t num = 0;
+    az_status_t res = az_uvarint_decode(uvarint, &num);
+
+    TEST_ASSERT_EQUAL_INT(AZ_STATUS_OK, res);
+    TEST_ASSERT_EQUAL_UINT64(127, num);
+}
+
+void test_az_uvarint_decode_specsample3(void)
+{
+    az_uvarint_t uvarint;
+    uvarint.len = 2;
+    uvarint.bytes = calloc(uvarint.len, sizeof(uint64_t));
+    uvarint.bytes[0] = 128;
+    uvarint.bytes[1] = 1;
+
+    uint64_t num = 0;
+    az_status_t res = az_uvarint_decode(uvarint, &num);
+
+    TEST_ASSERT_EQUAL_INT(AZ_STATUS_OK, res);
+    TEST_ASSERT_EQUAL_UINT64(128, num);
+}
+
+void test_az_uvarint_decode_specsample4(void)
+{
+    az_uvarint_t uvarint;
+    uvarint.len = 2;
+    uvarint.bytes = calloc(uvarint.len, sizeof(uint64_t));
+    uvarint.bytes[0] = 255;
+    uvarint.bytes[1] = 1;
+
+    uint64_t num = 0;
+    az_status_t res = az_uvarint_decode(uvarint, &num);
+
+    TEST_ASSERT_EQUAL_INT(AZ_STATUS_OK, res);
+    TEST_ASSERT_EQUAL_UINT64(255, num);
+}
+
+void test_az_uvarint_decode_specsample5(void)
+{
+    az_uvarint_t uvarint;
+    uvarint.len = 2;
+    uvarint.bytes = calloc(uvarint.len, sizeof(uint64_t));
+    uvarint.bytes[0] = 172;
+    uvarint.bytes[1] = 2;
+
+    uint64_t num = 0;
+    az_status_t res = az_uvarint_decode(uvarint, &num);
+
+    TEST_ASSERT_EQUAL_INT(AZ_STATUS_OK, res);
+    TEST_ASSERT_EQUAL_UINT64(300, num);
+}
+
+void test_az_uvarint_decode_specsample6(void)
+{
+    az_uvarint_t uvarint;
+    uvarint.len = 3;
+    uvarint.bytes = calloc(uvarint.len, sizeof(uint64_t));
+    uvarint.bytes[0] = 128;
+    uvarint.bytes[1] = 128;
+    uvarint.bytes[2] = 1;
+
+    uint64_t num = 0;
+    az_status_t res = az_uvarint_decode(uvarint, &num);
+
+    TEST_ASSERT_EQUAL_INT(AZ_STATUS_OK, res);
+    TEST_ASSERT_EQUAL_UINT64(16384, num);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
+   
+    /* az_uvarint_encode */
     RUN_TEST(test_az_uvarint_encode_specsample1);
     RUN_TEST(test_az_uvarint_encode_specsample2);
     RUN_TEST(test_az_uvarint_encode_specsample3);
     RUN_TEST(test_az_uvarint_encode_specsample4);
     RUN_TEST(test_az_uvarint_encode_specsample5);
     RUN_TEST(test_az_uvarint_encode_specsample6);
+
+    /* az_uvarint_decode */
+    RUN_TEST(test_az_uvarint_decode_specsample1);
+    RUN_TEST(test_az_uvarint_decode_specsample2);
+    RUN_TEST(test_az_uvarint_decode_specsample3);
+    RUN_TEST(test_az_uvarint_decode_specsample4);
+    RUN_TEST(test_az_uvarint_decode_specsample5);
+    RUN_TEST(test_az_uvarint_decode_specsample6);
+   
     return UNITY_END();
 }
 
