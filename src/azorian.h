b858e3d64f6bb9d2dc17d4818bde2d02356b8e84
jmcph4 <jmcph4@users.noreply.github.com>
2018-12-29 09:53:23 +1000

    Implement semantic versioning
    This commit implements semantic versioning for Azorian and adds macro
    definitions for programmatic access to version information.

---
+++ src/azorian.h
@@ -0,0 +1,10 @@
+#ifndef AZORIAN_H_
+#define AZORIAN_H_
+
+/* semantic versioning information */
+#define AZORIAN_VERSION_MAJOR 0
+#define AZORIAN_VERSION_MINOR 1
+#define AZORIAN_VERSION_PATCH 0
+
+#endif /* AZORIAN_H_ */
+
