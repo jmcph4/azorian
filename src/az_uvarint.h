822b821a17ff49c4e9fa9dc4c49821f08430c284
jmcph4 <jmcph4@users.noreply.github.com>
2018-12-29 09:48:40 +1000

    Remove C++ linking preprocessor directives
    This commit removes conditional `extern "C"` statements from the
    unsigned variable integer implementation. This was a holdover from an
    earlier attempt to use Google Test.

--- src/az_uvarint.h
+++ src/az_uvarint.h
@@ -25,16 +25,8 @@ typedef struct
     uint8_t* bytes;     /* bytes themselves */
 } az_uvarint_t;
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 az_status_t az_uvarint_encode(uint64_t num, az_uvarint_t* uvarint);
 az_status_t az_uvarint_decode(az_uvarint_t uvarint, uint64_t* num);
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
 
 #endif /* AZ_UVARINT_H_ */
 
