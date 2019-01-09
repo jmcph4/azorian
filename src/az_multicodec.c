8566600c66261fca3eb176879fe068a032db4586
jmcph4 <jmcph4@users.noreply.github.com>
2019-01-09 18:54:21 +1000

    Add multicodec implementation
    This commit adds a basic multicodec implementation via the
    `az_multicodec_t` type.

---
+++ src/az_multicodec.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+#include "az_status.h"
+#include "az_uvarint.h"
+#include "az_multicodec.h"
+
+/* private helper function to convert a given native integer into the correct
+ * multicodec type (good candidate for macro conversion */
+az_multicodec_t make_mc(uintmax_t x)
+{
+    az_multicodec_t mc;
+
+    az_status_t res = az_uvarint_encode(x, &mc);
+
+    if(res != AZ_STATUS_OK)
+    {
+        return (az_multicodec_t){0};
+    }
+    
+    return mc;
+}
+
--- src/azorian.h
+++ src/azorian.h
@@ -3,7 +3,7 @@
 
 /* semantic versioning information */
 #define AZORIAN_VERSION_MAJOR 0
-#define AZORIAN_VERSION_MINOR 2
+#define AZORIAN_VERSION_MINOR 3
 #define AZORIAN_VERSION_PATCH 0
 
 #endif /* AZORIAN_H_ */
