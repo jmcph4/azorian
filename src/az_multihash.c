d297168c848da2d0cc953cdbd7b02381172e400a
jmcph4 <jmcph4@users.noreply.github.com>
2019-01-12 14:26:40 +1000

    Add Doxygen comments to multihash functions
    This commit adds missing Doxygen comments to the multihash
    implementation (in `src/az_multihash.c`).

--- src/az_multihash.c
+++ src/az_multihash.c
@@ -1,3 +1,10 @@
+/**
+ * @file az_multihash.c
+ *
+ * Contains implementations of API functions in the form of function
+ * definitions.
+ *
+ * */
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -6,6 +13,28 @@
 #include "az_multicodec.h"
 #include "az_multihash.h"
 
+/**
+ * Initialises and populates a <code>az_multihash_t</code> type from the
+ * provided fields.
+ *
+ * @param hash_type
+ *          a <code>a_multicodec_t</code> type representing the type of hash
+ *          digest being represented
+ * @param digest_size
+ *          a native integer type representing the number of bytes of digest
+ *          data to be expected
+ * @param digest
+ *          a pointer to an array of bytes representing the digest data to be
+ *          encoded
+ * @param multihash
+ *          a pointer to a <code>az_multihash_t</code> type to store the
+ *          encoded multihash values in
+ * @return <code>az_status_t</code> result type indicating success or failure
+ * @throw AZ_ERR_ILLEGAL_PARAM
+ *          if <code>digest == NULL || multihash == NULL</code>
+ * @see <code>az_multihash_decode</code>
+ *
+ * */
 az_status_t az_multihash_encode(az_multicodec_t hash_type,
         uintmax_t digest_size, uint8_t* digest, az_multihash_t* multihash)
 {
@@ -40,6 +69,28 @@ az_status_t az_multihash_encode(az_multicodec_t hash_type,
     return AZ_STATUS_OK;
 }
 
+/**
+ * Extracts the fields from a <code>az_multihash_t</code> type.
+ *
+ * @param multihash
+ *          the <code>az_multihash_t</code> type containing the multihash
+ * @param hash_type
+ *          a pointer to a <code>a_multicodec_t</code> type representing the
+ *          type of digest being represented
+ * @param digest_size
+ *          a pointer to a native integer type representing the number of bytes
+ *          of digest data to be expected
+ * @param digest
+ *          a double-indirection pointer to an array of bytes that will contain
+ *          the digest data
+ * @return <code>az_status_t</code> result type indicating success or failure
+ * @throw AZ_ERR_ILLEGAL_PARAM
+ *          if <code>digest == NULL || multihash == NULL</code>
+ * @throw AZ_ERR_ALLOC_FAILURE
+ *          if allocation of target digest buffer fails
+ * @see <code>az_multihash_encode</code>
+ *
+ * */
 az_status_t az_multihash_decode(az_multihash_t multihash,
         az_multicodec_t* hash_type, uintmax_t* digest_size, uint8_t** digest)
 {
