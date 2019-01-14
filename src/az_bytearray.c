df0ef47ff1a91a706fe735289a87542da4682c73
jmcph4 <jmcph4@users.noreply.github.com>
2019-01-14 18:10:54 +1000

    Improve Doxygen comments in bytearray implementation
    This commit adds additional Doxygen comments to the bytearray
    implementation.

--- src/az_bytearray.c
+++ src/az_bytearray.c
@@ -1,3 +1,9 @@
+/**
+ * @file az_bytearray.c
+ *
+ * Contains implementation of the bytearray API via function definitions.
+ *
+ * */
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,7 +21,12 @@
  *          the data to be stored in the bytearray
  * @param bytearray
  *          pointer to a <code>az_bytearray_t</code> type to be initialised
- *
+ * @return <code>az_status_t</code> type indicating result of operation
+ * @throw AZ_ERR_ILLEGAL_PARAM
+ *          if <code>data == NULL || bytearray == NULL</code>
+ * @throw AZ_ERR_ALLOC_FAILURE
+ *          if memory allocation fails
+ * @see <code>az_bytearray_free</code>
  *
  * */
 az_status_t az_bytearray_init(size_t len, uint8_t* data,
@@ -45,7 +56,10 @@ az_status_t az_bytearray_init(size_t len, uint8_t* data,
  *
  * @param bytearray
  *          the <code>az_bytearray_t</code> type to be deallocated
- * 
+ * @return <code>az_status_t</code> type indicating result of operation
+ * @throw AZ_ERR_ILLEGAL_PARAM
+ *          if <code>bytearray == NULL</code>
+ * @see <code>az_bytearray_init</code> 
  * 
  * */
 az_status_t az_bytearray_free(az_bytearray_t* bytearray)
@@ -63,6 +77,22 @@ az_status_t az_bytearray_free(az_bytearray_t* bytearray)
 }
 
 /* Equality */
+
+/**
+ * Determines whether two bytearrays, <code>a</code> and <code>b</code>, are
+ * equal.
+ *
+ * @param a
+ *          the first bytearray to compare
+ * @param b
+ *          the second bytearray to compare
+ * @param equal
+ *          pointer to a <code>bool</code>, indicating equality
+ * @return <code>az_status_t</code> indicating success of operation
+ * @throw AZ_ERR_ILLEGAL_PARAM
+ *          if <code>equal == NULL</code>
+ *
+ * */
 az_status_t az_bytearray_equal(az_bytearray_t a, az_bytearray_t b, bool* equal)
 {
     if(equal == NULL) /* null guard */
--- src/az_bytearray.h
+++ src/az_bytearray.h
@@ -1,17 +1,25 @@
+/**
+ * @file az_bytearray.h
+ *
+ * Contains API definitions for the bytearray implementation in the form of
+ * function declarations.
+ *
+ * */
 #ifndef AZ_BYTEARRAY_H_
 #define AZ_BYTEARRAY_H_
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 /**
- *
+ * Represents an array of bytes, storing arbitrary binary data.
  *
  * */
 typedef struct
 {
-    size_t len;
-    uint8_t* data;
+    size_t len;         /* number of bytes stored */
+    uint8_t* data;      /* actual bytes */
 } az_bytearray_t;
 
 /* Initialisation */
